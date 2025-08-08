e41e826e84186467fdb5cf75b46914202f897379
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-08-08 14:22:05 +0545

    dne]

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -164,16 +164,16 @@ const Checkout = () => {
             <div className="mt-6 border-t border-b py-2">
               <div className="flex items-center justify-between">
                 <p className="text-sm font-medium text-gray-900">Subtotal</p>
-                <p className="font-semibold text-gray-900">Rs : {subTotal}</p>
+                <p className="font-semibold text-gray-900">Rs : {subTotal.toFixed(2)}</p>
               </div>
               <div className="flex items-center justify-between">
                 <p className="text-sm font-medium text-gray-900">Shipping</p>
-                <p className="font-semibold text-gray-900">Rs : {shippingAmount}</p>
+                <p className="font-semibold text-gray-900">Rs : {shippingAmount.toFixed(2)}</p>
               </div>
             </div>
             <div className="mt-6 flex items-center justify-between">
               <p className="text-sm font-medium text-gray-900">Total</p>
-              <p className="text-2xl font-semibold text-gray-900">Rs : {totalAmount}</p>
+              <p className="text-2xl font-semibold text-gray-900">Rs : {totalAmount.toFixed(2)}</p>
             </div>
           </div>
          {
